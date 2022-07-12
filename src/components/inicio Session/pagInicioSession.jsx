ae8a4142066c144d3269c11c619486bd0b6069da
TinoMaster <oscarmarcos2673@gmail.com>
2022-07-11 22:02:01 -0400

    terminado Proceso registro

--- src/components/inicio Session/pagInicioSession.jsx
+++ src/components/inicio Session/pagInicioSession.jsx
@@ -5,11 +5,17 @@ import Cookies from "universal-cookie";
 
 const Login = () => {
   const [form, setForm] = useState({});
+  const [registro, setRegistro] = useState({});
+  const [opcion, setOpcion] = useState("inicio");
+  const [succes, setSucces] = useState(null);
   const [error, setError] = useState(null);
 
   //Hadlers
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleChangeRegistro = (e) => {
+    setRegistro({ ...registro, [e.target.name]: e.target.value });
   };
 
   const handlerSubmit = async () => {
@@ -32,33 +38,106 @@ const Login = () => {
       });
   };
 
+  const enviarRagistro = async () => {
+    const data = {
+      nombre: registro.nombre,
+      usuario: registro.usuario,
+      contraseña: registro.contraseña,
+    };
+
+    let options = {
+      body: data,
+      headers: { "content-type": "application/json" },
+    };
+
+    if (registro.contraseña === registro.confirmacion) {
+      await httpHelper()
+        .post("http://localhost:5000/registro", options)
+        .then((res) => {
+          setSucces("Usuario creado correctmente");
+          setTimeout(() => {
+            window.location.reload();
+          }, 500);
+        })
+        .catch((err) => {
+          setError(err);
+        });
+    } else setError("Las contraseñas no coinciden");
+  };
+
   return (
-    <div className="w-screen flex-col relative h-screen flex justify-center bg-violet-600 items-center">
-      {error && <p className="p-2 top-36 text-red-500 bg-white rounded-lg absolute">{error.message}</p>}
-      <div className="w-11/12 md:w-1/5 flex flex-col overflow-hidden bg-white p-6 shadow-lg rounded-lg">
+    <div className="w-screen relative h-screen flex flex-wrap justify-center bg-violet-600">
+      {error && (
+        <p className="p-2 top-36 text-red-500 bg-white rounded-lg absolute">
+          {error.message}
+        </p>
+      )}
+      {succes && (
+        <p className="p-2 top-36 text-green-500 bg-white rounded-lg absolute">
+          {succes}
+        </p>
+      )}
+
+      <div className="flex items-start justify-center w-full">
+        <p
+          onClick={() => setOpcion("registro")}
+          className="p-4 rounded-md bg-white"
+        >
+          registro
+        </p>
+        <p
+          onClick={() => setOpcion("inicio")}
+          className="p-4 rounded-md bg-white"
+        >
+          inicio
+        </p>
+      </div>
+
+      <div className="w-11/12 md:w-1/5 h-2/3 flex flex-col overflow-hidden bg-white p-6 shadow-lg rounded-lg">
         <div className="w-full h-80 flex items-center justify-center">
           <img src={img} alt="Logo" />
         </div>
         <input
           type="email"
-          name="usuario"
-          placeholder="Usuario"
+          name="nombre"
+          placeholder="Nombre"
           className="w-3/5 mx-auto my-2 shadow-md p-2 rounded-md focus:outline-violet-300"
           autoComplete="off"
-          onChange={handleChange}
+          onChange={opcion === "inicio" ? handleChange : handleChangeRegistro}
         />
+        {opcion === "registro" && (
+          <input
+            type="email"
+            name="usuario"
+            placeholder="Correo"
+            className="w-3/5 mx-auto my-2 shadow-md p-2 rounded-md focus:outline-violet-300"
+            autoComplete="off"
+            onChange={handleChangeRegistro}
+          />
+        )}
+
         <input
           type="password"
           name="contraseña"
           placeholder="Contraseña"
           className="w-3/5 mx-auto my-2 shadow-md p-2 rounded-md focus:outline-violet-300"
-          onChange={handleChange}
+          onChange={opcion === "inicio" ? handleChange : handleChangeRegistro}
         />
+        {opcion === "registro" && (
+          <input
+            type="password"
+            name="confirmacion"
+            placeholder="Confirmar contraseña"
+            className="w-3/5 mx-auto my-2 shadow-md p-2 rounded-md focus:outline-violet-300"
+            onChange={handleChangeRegistro}
+          />
+        )}
+
         <input
           type="button"
-          value="Guardar"
+          value={opcion === "inicio" ? "Entrar" : "Registrar"}
           className="mt-2 p-2 bg-violet-600 text-white rounded-md shadow-md shadow-violet-500/75 hover:cursor-pointer hover:bg-violet-700"
-          onClick={handlerSubmit}
+          onClick={opcion === "inicio" ? handlerSubmit : enviarRagistro}
         />
       </div>
     </div>
