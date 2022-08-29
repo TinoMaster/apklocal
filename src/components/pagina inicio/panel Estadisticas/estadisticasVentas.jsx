aba2d8a3ecf6c5bfd5103593530cc2ae70edccba
TinoMaster <oscarmarcos2673@gmail.com>
2022-08-28 21:20:16 -0500

    arreglo de bugs

---
+++ src/components/pagina inicio/panel Estadisticas/estadisticasVentas.jsx
@@ -0,0 +1,111 @@
+import {
+  faCalendar,
+  faDollar,
+  faScaleBalanced,
+  faUserTie,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+export const EstadisticasVentas = ({
+  mejorVenta,
+  bdCuadre,
+  mejorYpeorMes,
+  mejorTurno,
+}) => {
+  return (
+    <>
+      <div className="m-2 shadow-md p-2 rounded-md flex flex-wrap justify-between">
+        <h4 className="md:text-lg text-gray-700 font-serif flex">
+          <FontAwesomeIcon
+            className="text-white bg-gray-500 mr-2 rounded-full p-1"
+            icon={faScaleBalanced}
+          />
+          Mejor Venta
+        </h4>
+        <p className="text-sm md:text-base text-green-700">
+          <FontAwesomeIcon icon={faDollar} /> {mejorVenta(bdCuadre).miron}
+        </p>
+        <div className="w-full flex">
+          <div className="w-1/2 flex p-3 justify-center">
+            <p className="text-xs font-serif text-slate-700">Fecha:</p>
+            <p className="text-xs ml-1 text-violet-600">
+              {mejorVenta(bdCuadre).fecha}
+            </p>
+          </div>
+          <div className="w-1/2 flex p-3 justify-center">
+            <p className="text-xs font-serif text-slate-700">Turno:</p>
+            {mejorVenta(bdCuadre) && (
+              <p className="text-xs ml-1 text-violet-600">{`${
+                mejorVenta(bdCuadre).turno.trabajador1
+              } y ${mejorVenta(bdCuadre).turno.trabajador2}`}</p>
+            )}
+          </div>
+        </div>
+      </div>
+      {/* Mejor Mes */}
+      <div className="m-2 shadow-md p-2 rounded-md flex flex-wrap justify-between">
+        <h4 className="md:text-lg text-gray-700 font-serif flex">
+          <FontAwesomeIcon
+            className="text-white bg-gray-500 mr-2 rounded-full p-1"
+            icon={faCalendar}
+          />
+          Mejor Mes
+        </h4>
+        <p className="text-sm md:text-base font-serif text-green-700">
+          {mejorYpeorMes(bdCuadre)[0] ? mejorYpeorMes(bdCuadre)[0] : 0}
+        </p>
+        <div className="w-full flex">
+          <div className="w-full flex p-3 justify-center">
+            <p className="text-xs font-serif text-slate-700">Venta:</p>
+            <p className="text-xs ml-1 text-violet-600">{`$${
+              mejorYpeorMes(bdCuadre)[1]
+            }`}</p>
+          </div>
+        </div>
+      </div>
+      {/* Peor mes */}
+      <div className="m-2 shadow-md p-2 rounded-md flex flex-wrap justify-between">
+        <h4 className="md:text-lg text-gray-700 font-serif flex">
+          <FontAwesomeIcon
+            className="text-white bg-gray-500 mr-2 rounded-full p-1"
+            icon={faCalendar}
+          />
+          Peor Mes
+        </h4>
+        <p className="text-sm md:text-base text-red-600/80 font-serif">
+          {mejorYpeorMes(bdCuadre)[2]}
+        </p>
+        <div className="w-full flex">
+          <div className="w-full flex p-3 justify-center">
+            <p className="text-xs font-serif text-slate-700">Venta:</p>
+            <p className="text-xs ml-1 text-violet-600">{`$${
+              mejorYpeorMes(bdCuadre)[3]
+            }`}</p>
+          </div>
+        </div>
+      </div>
+      {/* Mejor Turno */}
+      <div className="m-2 shadow-md p-2 rounded-md flex flex-wrap justify-between">
+        <h4 className="md:text-lg text-gray-700 font-serif flex">
+          <FontAwesomeIcon
+            className="text-white bg-gray-500 mr-2 rounded-full p-1"
+            icon={faUserTie}
+          />
+          Mejor Turno
+        </h4>
+        <p className="text-sm md:text-base text-green-700">
+          {mejorTurno(bdCuadre)[0]}
+        </p>
+        <div className="w-full flex">
+          <div className="w-full flex p-3 justify-center">
+            <p className="text-xs font-serif text-slate-700">Vendido:</p>
+            <p className="text-xs ml-1 text-violet-600">
+              {mejorTurno(bdCuadre)[1]}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/components/pagina ventas/pagVentas.jsx
+++ src/components/pagina ventas/pagVentas.jsx
@@ -1,8 +1,6 @@
 import {
   faCalendarDays,
   faDollarSign,
-  faPenToSquare,
-  faTrashCan,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useContext } from "react";
