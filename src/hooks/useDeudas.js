863c99cbc60bcb5ba48151323f631f803f8e94fe
TinoMaster <oscarmarcos2673@gmail.com>
2023-06-14 18:09:31 +0200

    ok

--- src/hooks/useDeudas.js
+++ src/hooks/useDeudas.js
@@ -2,19 +2,37 @@ import { useEffect, useState } from "react";
 import { httpHelper } from "../helpers/httpHelper";
 import dbConfig from "../config/api.config.json";
 
+const initialState = {
+  name: "",
+  creador: "",
+  deudor: "",
+  acreedor: "",
+  deuda: 0,
+  fecha: "",
+  pagada: {
+    isDone: false,
+    fecha: "",
+  },
+  pagos: [],
+};
+
 export const useDeudas = () => {
   /* General States */
   const [deudas, setDeudas] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState({});
+  /* Data to send */
+  const [createDeuda, setCreateDeuda] = useState(initialState);
   /* Modal States */
   const [modalCreateCollection, setModalCreateCollection] = useState(false);
+  const [modalCreateDebt, setModalCreateDebt] = useState(true);
 
   useEffect(() => {
     setLoading(true);
     httpHelper()
       .get(`${dbConfig.api.url}/deudas`)
       .then((res) => {
+        console.log(res);
         if (!res.success) {
           setLoading(false);
           setError(res);
@@ -26,7 +44,18 @@ export const useDeudas = () => {
       });
   }, []);
 
-  const modales = { modalCreateCollection, setModalCreateCollection };
+  const handlerCreateDebt = (e) => {
+    const { name, value } = e.target;
+    setCreateDeuda({ ...createDeuda, [name]: value });
+  };
 
-  return { deudas, error, loading, modales };
+  const modales = {
+    modalCreateCollection,
+    setModalCreateCollection,
+    setModalCreateDebt,
+    modalCreateDebt,
+  };
+  const handlerFunctions = { handlerCreateDebt };
+
+  return { deudas, error, loading, modales, handlerFunctions };
 };
